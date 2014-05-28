1941c0793652c3035a0fb290887ed32565d2f4d4
Martin Poelstra <martin@beryllium.net>
2014-05-29 00:29:01 +0200

    Fix parsing of comments for unix line endings.

--- src/typedoc/factories/handlers/CommentHandler.ts
+++ src/typedoc/factories/handlers/CommentHandler.ts
@@ -148,7 +148,7 @@ module TypeDoc.Factories
 
             var currentTag:Models.CommentTag;
             var shortText:number = 0;
-            var lines = text.split(/\r\n/);
+            var lines = text.split(/\n/);
             lines.forEach((line) => {
                 line = line.replace(/^\s*\*? ?/, '');
                 line = line.replace(/\s*$/, '');
