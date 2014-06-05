9bfdb2136e05467a62566c3782ec4bc6048584e0
Sebastian Lenz <sebastian@sebastian-lenz.de>
2014-06-05 23:36:32 +0200

    Added exclude option

--- src/typescript/bootstrap.ts
+++ src/typescript/bootstrap.ts
@@ -1,16 +1,22 @@
-/// <reference path="../lib/typescript/typescript.d.ts" />
+/// <reference path="../lib/fs.extra/fs.extra.d.ts" />
+/// <reference path="../lib/handlebars/handlebars.d.ts" />
+/// <reference path="../lib/highlight.js/highlight.js.d.ts" />
+/// <reference path="../lib/marked/marked.d.ts" />
+/// <reference path="../lib/minimatch/minimatch.d.ts" />
 /// <reference path="../lib/node/node.d.ts" />
+/// <reference path="../lib/typescript/typescript.d.ts" />
 
 declare module TypeScript {
     var typescriptPath:string;
 }
 
-var Handlebars = require('handlebars');
-var Marked     = require('marked');
+var Handlebars  = require('handlebars');
+var Marked      = require('marked');
 var HighlightJS = require('highlight.js');
-var VM         = require('vm');
-var Path       = require('path');
-var FS         = require('fs.extra');
+var Minimatch   = require('minimatch');
+var VM          = require('vm');
+var Path        = require('path');
+var FS          = require('fs.extra');
 
 var dirname = Path.dirname(require.resolve('typescript'));
 var file = Path.resolve(dirname, 'typescript.js');
