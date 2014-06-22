180e56a98ceba118a1f8cbeb3c32c5bcfd3397b9
Sebastian Lenz <sebastian@sebastian-lenz.de>
2014-06-22 15:48:23 +0200

    Add tsd.json

--- src/@init.ts
+++ src/@init.ts
@@ -1,23 +1,17 @@
-/// <reference path="lib/fs-extra/fs-extra.d.ts" />
-/// <reference path="lib/handlebars/handlebars.d.ts" />
-/// <reference path="lib/highlight.js/highlight.js.d.ts" />
-/// <reference path="lib/marked/marked.d.ts" />
-/// <reference path="lib/minimatch/minimatch.d.ts" />
-/// <reference path="lib/node/node.d.ts" />
-/// <reference path="lib/typescript/typescript.d.ts" />
+/// <reference path="lib/tsd.d.ts" />
 
 declare module TypeScript {
     var typescriptPath:string;
 }
 
-var Handlebars  = require('handlebars');
-var Marked      = require('marked');
+var Handlebars:HandlebarsStatic = require('handlebars');
+var Marked:MarkedStatic = require('marked');
 var HighlightJS = require('highlight.js');
-var Minimatch   = require('minimatch');
-var Util        = require('util');
-var VM          = require('vm');
-var Path        = require('path');
-var FS          = require('fs-extra');
+var Minimatch = require('minimatch');
+var Util = require('util');
+var VM = require('vm');
+var Path = require('path');
+var FS = require('fs-extra');
 
 var typeScriptPath = Path.dirname(require.resolve('typescript'));
 if (!FS.existsSync(Path.resolve(typeScriptPath, 'typescript.js'))) {
