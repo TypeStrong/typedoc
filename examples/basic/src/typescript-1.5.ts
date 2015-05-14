611b615e4efb0b9af7682d898341ab0c1e3932bf
Sebastian Lenz <github@sebastian-lenz.de>
2015-05-14 19:51:40 +0200

    Add TypeScript 1.5 test to basix example

---
+++ examples/basic/src/typescript-1.5.ts
@@ -0,0 +1,26 @@
+/**
+ * Destructuring objects.
+ */
+var {destructObjectA, destructObjectB, destructObjectC} = {destructObjectA:0, destructObjectB:'string', destructObjectC:0};
+
+
+/**
+ * Destructuring arrays.
+ */
+var [destructArrayA, destructArrayB, destructArrayC = 10] = [0, 'string', 0];
+
+/**
+ * Array Destructuring with rest
+ */
+var [destructArrayWithRestA, destructArrayWithRestB, ...destructArrayWithRest] = [1, 2, 3, 4];
+
+/**
+ * Array Destructuring with ignores
+ */
+var [destructArrayWithIgnoresA, , ...destructArrayWithIgnoresRest] = [1, 2, 3, 4];
+
+/**
+ * Destructuring function parameters.
+ */
+function drawText({text = "", location:[x, y] = [0, 0], bold = false}) {
+}
