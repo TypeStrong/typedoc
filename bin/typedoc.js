81d8475606ddefc98ac64236ed00e1a5f271de63
Sebastian Lenz <github@sebastian-lenz.de>
2015-07-23 12:19:50 +0200

    Update to version 0.3.7

--- bin/typedoc.js
+++ bin/typedoc.js
@@ -548,7 +548,7 @@ var td;
         /**
          * The version number of TypeDoc.
          */
-        Application.VERSION = '0.3.6';
+        Application.VERSION = '0.3.7';
         return Application;
     })(td.EventDispatcher);
     td.Application = Application;
