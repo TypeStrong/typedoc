839fe035fdee6b6e11caeabb3913194a9dda3301
Emiliano Santucci <emiliano.santucci@gmail.com>
2016-10-12 23:43:35 +0200

    Ignore new TypeScript 2.0 options
    Ignore new TypeScript 2.0 options until official typedoc can't manage it

--- src/lib/utils/options/sources/typescript.ts
+++ src/lib/utils/options/sources/typescript.ts
@@ -17,6 +17,8 @@ export class TypeScriptSource extends OptionsComponent
         'out', 'version', 'help',
         'watch', 'declaration', 'mapRoot',
         'sourceMap', 'inlineSources', 'removeComments'
+        // Ignore new TypeScript 2.0 options until official typedoc can't manage it
+        , 'paths', 'lib'
     ];
 
 
