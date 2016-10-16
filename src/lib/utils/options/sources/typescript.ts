ac4ec33340a50e4adf56e6e5d4bc6a9630b17ae8
Emiliano Santucci <emiliano.santucci@gmail.com>
2016-10-16 12:41:33 +0200

    Add unsupported TS 2.0 options

--- src/lib/utils/options/sources/typescript.ts
+++ src/lib/utils/options/sources/typescript.ts
@@ -17,8 +17,10 @@ export class TypeScriptSource extends OptionsComponent
         'out', 'version', 'help',
         'watch', 'declaration', 'mapRoot',
         'sourceMap', 'inlineSources', 'removeComments'
-        // Ignore new TypeScript 2.0 options until official typedoc can't manage it
-        , 'paths', 'lib'
+        // Ignore new TypeScript 2.0 options until typedoc can't manage it
+        , 'baseUrl', 'paths', 'lib', 'strictNullChecks', 'noImplicitThis',
+        'traceResolution', 'noUnusedParameters', 'noUnusedLocals',
+        'skipLibCheck', 'declarationDir'
     ];
 
 
