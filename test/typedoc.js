6ddb7f59766aa0929a94f170dd9165921925036b
Dennis Ploeger <github.dennis@dieploegers.de>
2017-02-13 19:46:46 +0100

    Honor the `--exclude` option on a list of files (#387)

--- test/typedoc.js
+++ test/typedoc.js
@@ -16,5 +16,22 @@ describe('TypeDoc', function() {
             Assert.notEqual(expanded.indexOf(Path.join(inputFiles, 'class.ts')), -1);
             Assert.equal(expanded.indexOf(inputFiles), -1);
         });
+        it('honors the exclude argument even on a fixed file list', function() {
+            var inputFiles = Path.join(__dirname, 'converter', 'class');
+            application.options.setValue('exclude', '**/class.ts');
+            var expanded = application.expandInputFiles([inputFiles]);
+
+            Assert.equal(expanded.indexOf(Path.join(inputFiles, 'class.ts')), -1);
+            Assert.equal(expanded.indexOf(inputFiles), -1);
+        });
+        it('supports multiple excludes', function() {
+            var inputFiles = Path.join(__dirname, 'converter');
+            application.options.setValue('exclude', '**/+(class|access).ts');
+            var expanded = application.expandInputFiles([inputFiles]);
+
+            Assert.equal(expanded.indexOf(Path.join(inputFiles, 'class', 'class.ts')), -1);
+            Assert.equal(expanded.indexOf(Path.join(inputFiles, 'access', 'access.ts')), -1);
+            Assert.equal(expanded.indexOf(inputFiles), -1);
+        });
     });
 });
