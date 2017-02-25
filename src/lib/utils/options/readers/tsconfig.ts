fd0c2d0c7105fcc32131b05bf034ef2d1c0ec66e
StrahilKazlachev <strahil.kazlachev@gmail.com>
2017-02-26 00:18:45 +0200

    feat(readers/tsconfig.ts): support "extends" close #380

--- src/lib/utils/options/readers/tsconfig.ts
+++ src/lib/utils/options/readers/tsconfig.ts
@@ -26,6 +26,30 @@ export class TSConfigReader extends OptionsComponent
     private static OPTIONS_KEY:string = 'tsconfig';
 
 
+    private readConfigFile(event:DiscoverEvent, fileName:string) {
+        if (!FS.existsSync(fileName)) {
+            event.addError('The tsconfig file %s does not exist.', fileName);
+            return;
+        }
+        let data = ts.readConfigFile(fileName, ts.sys.readFile).config;
+        if (data === undefined) {
+            event.addError('The tsconfig file %s does not contain valid JSON.', fileName);
+            return;
+        }
+        if (!_.isPlainObject(data)) {
+            event.addError('The tsconfig file %s does not contain a JSON object.', fileName);
+            return;
+        }
+        if (data.extends) {
+            const parent = this.readConfigFile(event, Path.resolve(Path.dirname(fileName), `${data.extends}.json`));
+            if (!parent) { return; }
+            data.compilerOptions = _.extend(parent.compilerOptions, data.compilerOptions);
+            data = _.extend(parent, data);
+            delete data.extends;
+        }
+        return data;
+    }
+
 
     initialize() {
         this.listenTo(this.owner, DiscoverEvent.DISCOVER, this.onDiscover, -100);
@@ -52,20 +76,9 @@ export class TSConfigReader extends OptionsComponent
      * @param fileName  The absolute path and file name of the tsconfig file.
      */
     load(event:DiscoverEvent, fileName:string) {
-        if (!FS.existsSync(fileName)) {
-            event.addError('The tsconfig file %s does not exist.', fileName);
-            return;
-        }
+        let data = this.readConfigFile(event, fileName);
 
-        let data = ts.readConfigFile(fileName, ts.sys.readFile).config;
-        if (data === undefined) {
-            event.addError('The tsconfig file %s does not contain valid JSON.', fileName);
-            return;
-        }
-        if (!_.isPlainObject(data)) {
-            event.addError('The tsconfig file %s does not contain a JSON object.', fileName);
-            return;
-        }
+        if (!data) { return; }
 
         data = ts.parseJsonConfigFileContent(
             data,
@@ -75,7 +88,6 @@ export class TSConfigReader extends OptionsComponent
             Path.resolve(fileName));
 
         event.inputFiles = data.fileNames;
-
         const ignored = TypeScriptSource.IGNORED;
         let compilerOptions = _.clone(data.raw.compilerOptions);
         for (const key of ignored) {
