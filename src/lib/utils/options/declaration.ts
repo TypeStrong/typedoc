6714c0d148369ac1627c553ad5969ca7b40ac342
Nick Nisi <nick@nisi.org>
2017-03-21 21:50:03 -0500

    adjust option declaration conversion for Map type

--- src/lib/utils/options/declaration.ts
+++ src/lib/utils/options/declaration.ts
@@ -97,8 +97,8 @@ export class OptionDeclaration {
                     const key = value ? (value + '').toLowerCase() : '';
                     const values = Object.keys(map).map(key => map[key]);
 
-                    if (map instanceof Map && map.has(key)) {
-                        value = map.get(key);
+                    if (map instanceof Map) {
+                        value = map.has(key) ? map.get(key) : key;
                     } else if (key in map) {
                         value = map[key];
                     } else if (values.indexOf(value) === -1 && errorCallback) {
