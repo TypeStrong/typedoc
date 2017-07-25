81779ebe417bf1bea1a8ed4b9c6c0dc93ac6d0f2
Kevin Eaton <kevineaton@users.noreply.github.com>
2017-07-25 14:34:43 -0400

    Check if the decorators are undefined in the export (#562)

--- src/lib/converter/nodes/export.ts
+++ src/lib/converter/nodes/export.ts
@@ -25,21 +25,25 @@ export class ExportConverter extends ConverterNodeComponent<ts.ExportAssignment>
         }
         if (symbol) {
             const project = context.project;
-            symbol.declarations.forEach((declaration) => {
-                if (!declaration.symbol) {
-                    return;
-                }
-                const id = project.symbolMapping[context.getSymbolID(declaration.symbol)];
-                if (!id) {
-                    return;
-                }
+            // if the symbol declarations are undefined due to an export, we skip it
+            // fixes https://github.com/TypeStrong/typedoc/issues/513
+            if (symbol.declarations) {
+                symbol.declarations.forEach((declaration) => {
+                    if (!declaration.symbol) {
+                        return;
+                    }
+                    const id = project.symbolMapping[context.getSymbolID(declaration.symbol)];
+                    if (!id) {
+                        return;
+                    }
 
-                const reflection = project.reflections[id];
-                if (node.isExportEquals && reflection instanceof DeclarationReflection) {
-                    (<DeclarationReflection> reflection).setFlag(ReflectionFlag.ExportAssignment, true);
-                }
-                markAsExported(reflection);
-            });
+                    const reflection = project.reflections[id];
+                    if (node.isExportEquals && reflection instanceof DeclarationReflection) {
+                        (<DeclarationReflection> reflection).setFlag(ReflectionFlag.ExportAssignment, true);
+                    }
+                    markAsExported(reflection);
+                });
+            }
         }
 
         function markAsExported(reflection: Reflection) {
