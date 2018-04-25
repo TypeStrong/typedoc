2cf1f5f48d41f5bc6c695a5c9074d5edf161ed53
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-04-25 23:33:18 +0200

    Correct posix pathing

--- src/lib/utils/paths.ts
+++ src/lib/utils/paths.ts
@@ -1,13 +1,12 @@
 import * as Path from 'path';
 import { Minimatch, IMinimatch } from 'minimatch';
 
+const unix = Path.sep === '/';
+
 export function createMinimatch(patterns: string[]): IMinimatch[] {
     return patterns.map((pattern: string): IMinimatch => {
-        // Ensure uniform absolute path cross OS
-        // (POSIX would resolve c:/path to /path/to/repo/c:/path without this check)
-        if (Path.isAbsolute(pattern) && Path.sep === '/') {
-            pattern = pattern.replace(/^\w:/, '');
-        }
+        // Ensure correct pathing on unix, by transforming `\` to `/` and remvoing any `X:/` fromt he path
+        if (unix) { pattern = pattern.replace(/[\\]/g, '/').replace(/^\w:/, ''); }
 
         // pattern paths not starting with '**' are resolved even if it is an
         // absolute path, to ensure correct format for the current OS
@@ -15,7 +14,10 @@ export function createMinimatch(patterns: string[]): IMinimatch[] {
             pattern = Path.resolve(pattern);
         }
 
+        // On Windows we transform `\` to `/` to unify the way paths are intepreted
+        if (!unix) { pattern = pattern.replace(/[\\]/g, '/'); }
+
         // Unify the path slashes before creating the minimatch, for more relyable matching
-        return new Minimatch(pattern.replace(/[\\]/g, '/'), { dot: true });
+        return new Minimatch(pattern, { dot: true });
     });
 }
