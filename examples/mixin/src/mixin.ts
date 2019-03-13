97ef64bd3a06755cad5db54101b8b9ab5569a56a
Nickolay <nickolay8@gmail.com>
2019-03-13 17:50:57 +0300

    better mixin example

--- examples/mixin/src/mixin.ts
+++ examples/mixin/src/mixin.ts
@@ -1,9 +1,21 @@
+/**
+ * Any function
+ */
 export type AnyFunction<A = any> = (...input: any[]) => A
+
+/**
+ * Any constructor function
+ */
 export type AnyConstructor<A = object> = new (...input: any[]) => A
 
+/**
+ * Mixin type helper
+ */
 export type Mixin<T extends AnyFunction> = InstanceType<ReturnType<T>>
 
-
+/**
+ * Base class
+ */
 export class Base {
     baseProperty    : string = 'init'
 
@@ -12,9 +24,14 @@ export class Base {
     }
 }
 
+/**
+ * The "mixin function" of the Mixin1
+ */
 export const Mixin1Func = <T extends AnyConstructor<Base>>(base : T) =>
 
-// internal mixin class
+/**
+ * Internal class of the Mixin1
+ */
 class Mixin1Class extends base {
     property1 : string = 'init'
 
@@ -24,17 +41,19 @@ class Mixin1Class extends base {
     }
 }
 
-// the "instance type" of this mixin
-// export type Mixin1 = Mixin<typeof Mixin1>
-
-// or, alternative notation (supports recursive type definition)
+/**
+ * The "instance type" of the Mixin1 using the interface notation (supports recursive type definition)
+ */
 export interface Mixin1Type extends Mixin<typeof Mixin1Func> {}
 
 
+/**
+ * The "mixin function" of the Mixin2
+ */
 export const Mixin2 = <T extends AnyConstructor<Mixin1Type & Base>>(base : T) =>
-
-
-// internal mixin class
+/**
+ * Internal class of the Mixin2
+ */
 class Mixin2 extends base {
     property2 : string = 'init'
 
@@ -44,14 +63,31 @@ class Mixin2 extends base {
     }
 }
 
-// the "instance type" of this mixin
-// export type Mixin1 = Mixin<typeof Mixin1>
-
-// or, alternative notation (supports recursive type definition)
+/**
+ * The "instance type" of the Mixin2 using the interface notation (supports recursive type definition)
+ */
 export interface Mixin2 extends Mixin<typeof Mixin2> {}
 
 
+/**
+ * The "mixin function" of the Mixin3
+ */
+export const Mixin3 = <T extends AnyConstructor<object>>(base : T) =>
+/**
+ * Internal class of the Mixin3
+ */
+class Mixin3 extends base {
+}
 
+/**
+ * The "instance type" of the Mixin3 using the regular type notation (does not work well for recursive type definition)
+ * Is not well supported by the TypeDoc
+ */
+export type Mixin3 = Mixin<typeof Mixin3>
+
+/**
+ * Class that inherits from Base and consumes Mixin1 and Mixin2, in order.
+ */
 export class SomeClassWithMixin extends Mixin2(Mixin1Func(Base)) {
     classWithMixinProperty  : string = 'init'
 
