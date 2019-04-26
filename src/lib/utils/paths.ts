fa16808df40db18a27776a34f8abfd8f2d8037dc
Michael Dokolin <dokmic@gmail.com>
2019-04-26 18:43:01 +0200

    fix: the bug with the negated/commented exclude patterns (#1025)
    * Fix negated patterns support (#1024)
    
    * Fix commented patterns support (#1024)
    
    * Fix assert messages (#1024)

--- src/lib/utils/paths.ts
+++ src/lib/utils/paths.ts
@@ -3,26 +3,30 @@ import { Minimatch, IMinimatch } from 'minimatch';
 
 const unix = Path.sep === '/';
 
+function normalize(pattern: string) {
+    if (pattern.startsWith('!') || pattern.startsWith('#')) {
+        return pattern[0] + normalize(pattern.substr(1));
+    }
+
+    if (unix) { pattern = pattern.replace(/[\\]/g, '/').replace(/^\w:/, ''); }
+
+    // pattern paths not starting with '**' are resolved even if it is an
+    // absolute path, to ensure correct format for the current OS
+    if (pattern.substr(0, 2) !== '**') {
+        pattern = Path.resolve(pattern);
+    }
+
+    // On Windows we transform `\` to `/` to unify the way paths are intepreted
+    if (!unix) { pattern = pattern.replace(/[\\]/g, '/'); }
+
+    return pattern;
+}
+
 /**
  * Convert array of glob patterns to array of minimatch instances.
  *
  * Handle a few Windows-Unix path gotchas.
  */
 export function createMinimatch(patterns: string[]): IMinimatch[] {
-    return patterns.map((pattern: string): IMinimatch => {
-        // Ensure correct pathing on unix, by transforming `\` to `/` and removing any `X:/` from the path
-        if (unix) { pattern = pattern.replace(/[\\]/g, '/').replace(/^\w:/, ''); }
-
-        // pattern paths not starting with '**' are resolved even if it is an
-        // absolute path, to ensure correct format for the current OS
-        if (pattern.substr(0, 2) !== '**') {
-            pattern = Path.resolve(pattern);
-        }
-
-        // On Windows we transform `\` to `/` to unify the way paths are intepreted
-        if (!unix) { pattern = pattern.replace(/[\\]/g, '/'); }
-
-        // Unify the path slashes before creating the minimatch, for more reliable matching
-        return new Minimatch(pattern, { dot: true });
-    });
+    return patterns.map(pattern => new Minimatch(normalize(pattern), { dot: true }));
 }
