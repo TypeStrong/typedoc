cdc458f19ac386725d5dd307ddbe2167d26cf3ff
Anthony Ciccarello <aciccarello@users.noreply.github.com>
2019-05-28 22:50:34 -0500

    update dependencies

--- examples/self/run.bat
+++ examples/self/run.bat
@@ -2,6 +2,6 @@
 set curr_dir=%cd%
 chdir /D "%~dp0"
 
-node ..\..\bin\typedoc --includeDeclarations --externalPattern **/lib/** --mode file --name "TypeDoc Documentation" --out doc\ ..\..\src\
+node ..\..\bin\typedoc --includeDeclarations --externalPattern **/lib/** --mode file --name "TypeDoc Documentation" --out doc\ ..\..\src\lib\
 
 chdir /D "%curr_dir%"
