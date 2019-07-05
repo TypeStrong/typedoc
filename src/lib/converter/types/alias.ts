07afa11f859afed5b13cdad8d30122b387df1be8
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-07-05 17:35:41 +0300

    fix: references marked as external if path has dots (#1052)
    * fix: references marked as external if path has dots
    
    Closes TypeStrong/typedoc#1049
    
    * refactor: ignore quotes in path
    
    * Remove quotes from path since it breaks on Windows

--- src/lib/converter/types/alias.ts
+++ src/lib/converter/types/alias.ts
@@ -33,12 +33,11 @@ export class AliasConverter extends ConverterTypeComponent implements TypeNodeCo
         }
 
         const checker = context.checker;
-        let symbolName = checker.getFullyQualifiedName(type.symbol).split('.');
+        const fqn = checker.getFullyQualifiedName(type.symbol);
+
+        let symbolName = fqn.replace(/".*"\./, '').split('.');
         if (!symbolName.length) {
             return false;
-        }
-        if (symbolName[0].substr(0, 1) === '"') {
-            symbolName.shift();
         }
 
         let nodeName = node.typeName.getText().split('.');
