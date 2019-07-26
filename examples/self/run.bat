9afb64a3c555568167c42e1de257b829bcb7b01a
Anthony Ciccarello <aciccarello@users.noreply.github.com>
2019-07-26 08:39:16 -0500

    align run scripts

--- examples/self/run.bat
+++ examples/self/run.bat
@@ -2,6 +2,6 @@
 set curr_dir=%cd%
 chdir /D "%~dp0"
 
-node ..\..\bin\typedoc --includeDeclarations --mode file --name "TypeDoc Documentation" --tsconfig ../../tsconfig.json --readme ../../README.md --out doc\ ..\..\src\lib\
+node ..\..\bin\typedoc --mode file --name "TypeDoc Documentation" --tsconfig ../../tsconfig.json --readme ../../README.md --out doc\ ..\..\src\lib\
 
 chdir /D "%curr_dir%"
