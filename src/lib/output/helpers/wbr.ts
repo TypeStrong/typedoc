5355abc164d5db8924698e2aef1190a081d0b659
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2019-10-27 12:23:38 -0600

    Escape text in wbr
    Fixes #780
    Supercedes TypeStrong/typedoc-default-themes#58

--- src/lib/output/helpers/wbr.ts
+++ src/lib/output/helpers/wbr.ts
@@ -1,16 +1,19 @@
+import * as Handlebars from 'handlebars';
+
 /**
  * Insert word break tags ``<wbr>`` into the given string.
  *
- * Breaks the given string at ``_``, ``-`` and captial letters.
+ * Breaks the given string at ``_``, ``-`` and capital letters.
  *
- * @param str  The string that should be split.
- * @return     The original string containing ``<wbr>`` tags where possible.
+ * @param str The string that should be split.
+ * @return The original string containing ``<wbr>`` tags where possible.
  */
-export function wbr(options: any): string {
+export function wbr(options: any): Handlebars.SafeString {
     let str = typeof options === 'string' ? options : options.fn(this);
+    str = Handlebars.escapeExpression(str);
 
     str = str.replace(/([^_\-][_\-])([^_\-])/g, (m: string, a: string, b: string) => a + '<wbr>' + b);
     str = str.replace(/([^A-Z])([A-Z][^A-Z])/g, (m: string, a: string, b: string) => a + '<wbr>' + b);
 
-    return str;
+    return new Handlebars.SafeString(str);
 }
