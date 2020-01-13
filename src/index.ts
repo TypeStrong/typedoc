5ec837a27aefd7e24a817e4dde7628202903ba9b
Alec LaLonde <alec@convergencelabs.com>
2020-01-13 15:02:38 -0700

    Extracted navigation building internal functions into an exported NavigationBuilder class

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { normalizePath } from './lib/utils/fs';
 export * from './lib/models/reflections';
 export * from './lib/output/plugins';
 export { Renderer } from './lib/output/renderer';
-export { DefaultTheme } from './lib/output/themes/DefaultTheme';
+export { DefaultTheme, NavigationBuilder } from './lib/output/themes/DefaultTheme';
 export { NavigationItem } from './lib/output/models/NavigationItem';
 export { UrlMapping } from './lib/output/models/UrlMapping';
 
