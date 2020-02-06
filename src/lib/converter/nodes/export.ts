1d3556da4087398d0b452f5e2c7727866cfe5d7c
JEROMEH <jerome.henaff@amdocs.com>
2020-02-07 00:37:55 +0100

    tslint fix

--- src/lib/converter/nodes/export.ts
+++ src/lib/converter/nodes/export.ts
@@ -10,7 +10,7 @@ import {
 import { Context } from '../context';
 import { Component, ConverterNodeComponent } from '../components';
 import { createReferenceReflection } from '../factories/reference';
-import {BindOption, SourceFileMode} from '../../utils';
+import { BindOption, SourceFileMode } from '../../utils';
 
 @Component({name: 'node:export'})
 export class ExportConverter extends ConverterNodeComponent<ts.ExportAssignment> {
