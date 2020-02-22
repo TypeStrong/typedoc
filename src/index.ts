0a0e50c6ac6ae440c9eb39fffe18898e3aaec61b
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-22 01:55:02 +0100

    Removed createMinimatch export from project in index and changed import path of the function in path tests

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { Application } from './lib/application';
 export { CliApplication } from './lib/cli';
 
 export { EventDispatcher, Event } from './lib/utils/events';
-export { createMinimatch } from './lib/utils/paths';
 export { resetReflectionID } from './lib/models/reflections/abstract';
 export { normalizePath } from './lib/utils/fs';
 export * from './lib/models/reflections';
