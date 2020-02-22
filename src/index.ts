8f587163ec3862c8744b6c9353c43e647a110d22
citycide <lingenbw@gmail.com>
2020-02-22 15:25:47 -0600

    reduce public API surface area to generic stringifier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Application } from './lib/application';
 export { CliApplication } from './lib/cli';
-export * from './lib/stringifiers';
+export { stringifyType } from './lib/stringifiers';
 
 export { EventDispatcher, Event } from './lib/utils/events';
 export { createMinimatch } from './lib/utils/paths';
