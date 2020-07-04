7fc721cd2333d70677837e97ca9ef4a3c134ab0e
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2020-07-04 10:08:45 -0600

    fix: Improve support for type aliases
    Fixes #1330

--- src/test/converter/alias/alias.ts
+++ src/test/converter/alias/alias.ts
@@ -33,3 +33,20 @@ export type HorribleRecursiveTypeThatShouldNotBeUsedByAnyone<T extends any[], R 
         [K in keyof R | keyof T[0]]: K extends keyof R ? R[K] : T[0][K]
     }>
 }[T['length'] extends 0 ? 0 : 1];
+
+namespace GH1330 {
+    type ExampleParam = Example
+    interface Example<T extends ExampleParam = ExampleParam> {}
+
+    declare const makeExample: () => Example
+    declare const makeExample2: () => ExampleParam
+
+    // Recursive type when we don't have a type node.
+    export const testValue = makeExample()
+    export const testValue2 = makeExample2()
+
+    type HasProp<T> = { key: T }
+
+    declare const makeProp: <T>(x: T) => HasProp<T>
+    export const testValue3 = makeProp(1)
+}
