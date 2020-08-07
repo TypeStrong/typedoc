134bae75085a0eeb7fcb10ec5bce8a012051b1c9
Krisztian Balla <k.balla@gmx.at>
2020-08-07 11:24:05 +0200

    Move methods to utils

--- src/lib/converter/nodes/class.ts
+++ src/lib/converter/nodes/class.ts
@@ -6,6 +6,7 @@ import { createDeclaration } from '../factories/index';
 import { Context } from '../context';
 import { Component, ConverterNodeComponent } from '../components';
 import { toArray } from 'lodash';
+import { getTypeArgumentsWithDefaults, getTypeParametersOfType } from '../utils/types';
 
 @Component({name: 'node:class'})
 export class ClassConverter extends ConverterNodeComponent<ts.ClassDeclaration> {
@@ -65,21 +66,17 @@ export class ClassConverter extends ConverterNodeComponent<ts.ClassDeclaration> 
                     const typesToInheritFrom: ts.Type[] = type.isIntersection() ? type.types : [ type ];
 
                     typesToInheritFrom.forEach((typeToInheritFrom: ts.Type) => {
+                        const typeParams = getTypeParametersOfType(typeToInheritFrom);
+                        const typeArguments = typeParams
+                            ? getTypeArgumentsWithDefaults(typeParams, baseType.typeArguments)
+                            : baseType.typeArguments;
+
                         // TODO: The TS declaration file claims that:
                         // 1. type.symbol is non-nullable
                         // 2. symbol.declarations is non-nullable
                         // These are both incorrect, GH#1207 for #2 and existing tests for #1.
                         // Figure out why this is the case and document.
                         typeToInheritFrom.symbol?.declarations?.forEach((declaration) => {
-                            let typeArguments = baseType.typeArguments;
-
-                            if (ts.isClassDeclaration(declaration) && declaration.typeParameters) {
-                                typeArguments = this.getTypeArgumentsWithDefaults(
-                                    declaration.typeParameters,
-                                    baseType.typeArguments
-                                );
-                            }
-
                             context.inherit(declaration, typeArguments);
                         });
                     });
@@ -94,33 +91,5 @@ export class ClassConverter extends ConverterNodeComponent<ts.ClassDeclaration> 
         });
 
         return reflection;
-    }
-
-    /**
-     * Returns a list of type arguments. If a type parameter has no corresponding type argument, the default type
-     * for that type parameter is used as the type argument.
-     * @param typeParams The type parameters for which the type arguments are wanted.
-     * @param typeArguments The type arguments as provided in the declaration.
-     * @returns The complete list of type arguments with possible default values if type arguments are missing.
-     */
-    getTypeArgumentsWithDefaults(
-        typeParams: ts.NodeArray<ts.TypeParameterDeclaration>,
-        typeArguments?: ts.NodeArray<ts.TypeNode>
-    ): ts.NodeArray<ts.TypeNode> {
-        if (!typeArguments || typeParams.length > typeArguments.length) {
-            const typeArgumentsWithDefaults = new Array<ts.TypeNode>();
-
-            for (let i = 0; i < typeParams.length; ++i) {
-                if (typeArguments && typeArguments[i]) {
-                    typeArgumentsWithDefaults.push(typeArguments[i]);
-                } else if (typeParams[i].default) {
-                    typeArgumentsWithDefaults.push(typeParams[i].default!);
-                }
-            }
-
-            return ts.createNodeArray<ts.TypeNode>(typeArgumentsWithDefaults);
-        }
-
-        return typeArguments;
     }
 }
---
+++ src/lib/converter/utils/types.ts
@@ -0,0 +1,44 @@
+import * as ts from 'typescript';
+
+/**
+ * Returns the type parameters of a given type.
+ * @param type The type whos type parameters are wanted.
+ * @returns The type parameters of the type.
+ */
+export function getTypeParametersOfType(type: ts.Type): ts.NodeArray<ts.TypeParameterDeclaration> | undefined {
+    type.symbol?.declarations?.forEach((declaration) => {
+        if (ts.isClassDeclaration(declaration)) {
+            return declaration.typeParameters;
+        }
+    });
+
+    return undefined;
+}
+
+/**
+ * Returns a list of type arguments. If a type parameter has no corresponding type argument, the default type
+ * for that type parameter is used as the type argument.
+ * @param typeParams The type parameters for which the type arguments are wanted.
+ * @param typeArguments The type arguments as provided in the declaration.
+ * @returns The complete list of type arguments with possible default values if type arguments are missing.
+ */
+export function getTypeArgumentsWithDefaults(
+    typeParams: ts.NodeArray<ts.TypeParameterDeclaration>,
+    typeArguments?: ts.NodeArray<ts.TypeNode>
+): ts.NodeArray<ts.TypeNode> {
+    if (!typeArguments || typeParams.length > typeArguments.length) {
+        const typeArgumentsWithDefaults = new Array<ts.TypeNode>();
+
+        for (let i = 0; i < typeParams.length; ++i) {
+            if (typeArguments && typeArguments[i]) {
+                typeArgumentsWithDefaults.push(typeArguments[i]);
+            } else if (typeParams[i].default) {
+                typeArgumentsWithDefaults.push(typeParams[i].default!);
+            }
+        }
+
+        return ts.createNodeArray<ts.TypeNode>(typeArgumentsWithDefaults);
+    }
+
+    return typeArguments;
+}
