49b9365a94b94f529e7fd8e8dfd71579b2df25ea
michaelf <michael.froeschen@aixpertsoft.de>
2021-02-18 14:38:56 +0100

    feat: preserve spaces in code blocks

---
+++ src/test/converter/comment/comment4.ts
@@ -0,0 +1,13 @@
+/**
+ * This is a comment containing a multiline code block
+ * ```ts
+ * export function multiply(a: number, b: number) {
+ *   return a * b;
+ * }
+ * ```
+ * @module
+ */
+
+export function multiply(a: number, b: number) {
+    return a * b;
+}
