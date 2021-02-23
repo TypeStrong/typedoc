adfa846ba17f3a90fe81515e160ba81e9ee7afe7
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2021-02-22 18:40:59 -0700

    chore: Disable publishing sourcemaps
    They are unnecessarily inflating the bundle size, and I never debug from a published bundle in the first place, so they aren't that useful.

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,7 +4,7 @@
  * script will be used to switch this flag to false when publishing, then immediately back
  * to true after a successful publish.
  */
-type InternalOnly = true;
+type InternalOnly = false;
 
 /**
  * Helper type to convert `T` to `F` if strict mode is on.
