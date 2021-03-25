02fe2a73048687288cb33d837ff685a4c6c1e2b4
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2021-03-24 21:26:12 -0600

    fix: Crash when converting recursive type alias
    Resolves #1547

--- src/lib/converter/types.ts
+++ src/lib/converter/types.ts
@@ -121,11 +121,6 @@ export function convertType(
         return requestBugReport(context, typeOrNode);
     }
 
-    // TS 4.2 added this to enable better tracking of type aliases.
-    if (typeOrNode.isUnion() && typeOrNode.origin) {
-        return convertType(context, typeOrNode.origin);
-    }
-
     // IgnoreErrors is important, without it, we can't assert that we will get a node.
     const node = context.checker.typeToTypeNode(
         typeOrNode,
@@ -952,6 +947,11 @@ const typeOperatorConverter: TypeConverter<ts.TypeOperatorNode> = {
         // keyof will only show up with generic functions, otherwise it gets eagerly
         // resolved to a union of strings.
         if (node.operator === ts.SyntaxKind.KeyOfKeyword) {
+            // TS 4.2 added this to enable better tracking of type aliases.
+            if (type.isUnion() && type.origin) {
+                return convertType(context, type.origin);
+            }
+
             // There's probably an interface for this somewhere... I couldn't find it.
             const targetType = (type as ts.Type & { type: ts.Type }).type;
             return new TypeOperatorType(
@@ -974,6 +974,11 @@ const unionConverter: TypeConverter<ts.UnionTypeNode, ts.UnionType> = {
         );
     },
     convertType(context, type) {
+        // TS 4.2 added this to enable better tracking of type aliases.
+        if (type.origin) {
+            return convertType(context, type.origin);
+        }
+
         return new UnionType(
             type.types.map((type) => convertType(context, type))
         );
--- src/test/converter/alias/alias.ts
+++ src/test/converter/alias/alias.ts
@@ -73,3 +73,31 @@ export namespace GH1454 {
     export declare function bar(x: Bar): Bar;
     export declare function foo(x: Foo): Foo;
 }
+
+export namespace GH1547 {
+    export interface ThingA {
+        type: "ThingA";
+    }
+
+    export interface ThingB {
+        type: "ThingB";
+    }
+
+    type Things = ThingA | ThingB;
+
+    type ValueOrArray<T> = T | Array<ValueOrArray<T>>;
+
+    /**
+     * Test.
+     */
+    export class Test {
+        /**
+         * Log a thing.
+         *
+         * @param things - Array of things or a thing.
+         */
+        log_thing(things: ValueOrArray<Things>): void {
+            console.log(things);
+        }
+    }
+}
