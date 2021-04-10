05243055f8cfe8359be50b8121bfdbb8d738a6c4
cAttte <26514199+cAttte@users.noreply.github.com>
2021-04-10 12:53:38 -0300

    refactor: access 'pretty' option directly from generateJson()

--- scripts/rebuild_specs.js
+++ scripts/rebuild_specs.js
@@ -107,7 +107,7 @@ async function rebuildRendererTest() {
     app.options.setValue("entryPoints", app.expandInputFiles([src]));
     const project = app.convert();
     await app.generateDocs(project, out);
-    await app.generateJson(project, path.join(out, "specs.json"), true);
+    await app.generateJson(project, path.join(out, "specs.json"));
 
     /**
      * Avoiding sync methods here is... difficult.
