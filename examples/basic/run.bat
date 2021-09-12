4c1abc1eff13ec4926127a0c838eade0885f4f92
Sam Magura <srmagura@gmail.com>
2021-09-12 13:51:49 -0400

    remove old options from example run & run.bat

--- examples/basic/run.bat
+++ examples/basic/run.bat
@@ -2,6 +2,6 @@
 set curr_dir=%cd%
 chdir /D "%~dp0"
 
-node ..\..\bin\typedoc --module commonjs --includes inc\ --media media\ --target ES5 --noLib --out doc\ src\
+node ..\..\bin\typedoc --includes inc\ --media media\ --json json.json --out doc\ src\
 
 chdir /D "%curr_dir%"
