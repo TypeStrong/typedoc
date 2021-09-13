40b5bd774f76c18061c8728678b187076a39d0ea
Sam Magura <srmagura@gmail.com>
2021-09-12 21:12:47 -0400

    fix examples/self run scripts

--- examples/self/run.bat
+++ examples/self/run.bat
@@ -2,6 +2,6 @@
 set curr_dir=%cd%
 chdir /D "%~dp0"
 
-node ..\..\bin\typedoc --mode file --name "TypeDoc Documentation" --tsconfig ../../tsconfig.json --readme ../../README.md --out doc\ ..\..\src\lib\
+node ..\..\bin\typedoc --name "TypeDoc Documentation" --tsconfig ../../tsconfig.json --readme ../../README.md --out doc\ --entryPointStrategy expand ..\..\src\lib\
 
 chdir /D "%curr_dir%"
