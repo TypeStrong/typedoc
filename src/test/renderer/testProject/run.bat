5cb6cb6e2f1a33ae2a6f53dffed0e6fc77d21b09
Sam Magura <srmagura@gmail.com>
2021-09-29 00:56:41 -0400

    1556: move examples/self to src/test/renderer/self

--- src/test/renderer/testProject/run.bat
+++ src/test/renderer/testProject/run.bat
@@ -2,6 +2,6 @@
 set curr_dir=%cd%
 chdir /D "%~dp0"
 
-node ..\..\bin\typedoc --includes inc\ --media media\ --json json.json --out doc\ src\
+node ..\..\..\..\bin\typedoc --includes inc\ --media media\ --json json.json --out doc\ src\
 
 chdir /D "%curr_dir%"
