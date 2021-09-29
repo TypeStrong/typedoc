801940d18b8858a2b6dbc7d3c1cd8d7b4179dc10
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2021-09-29 14:36:20 -0600

    Initial attempt at a comment lexer

---
+++ src/lib/converter/comments.ts
@@ -0,0 +1,315 @@
+export enum TokenSyntaxKind {
+    Text,
+    NewLine,
+    OpenBrace,
+    CloseBrace,
+    Tag,
+    Code,
+}
+
+export interface Token {
+    kind: TokenSyntaxKind;
+    text: string;
+}
+
+export function* lexBlockComment(
+    file: string,
+    pos = 0,
+    end = file.length
+): Generator<Token> {
+    // Wrapper around our real lex function to collapse adjacent text tokens.
+    let textToken: Token | undefined;
+    for (const token of lexBlockComment2(file, pos, end)) {
+        if (token.kind === TokenSyntaxKind.Text) {
+            if (textToken) {
+                textToken.text += token.text;
+            } else {
+                textToken = token;
+            }
+        } else {
+            if (textToken) {
+                yield textToken;
+                textToken = void 0;
+            }
+            yield token;
+        }
+    }
+
+    if (textToken) {
+        yield textToken;
+    }
+}
+
+function* lexBlockComment2(
+    file: string,
+    pos: number,
+    end: number
+): Generator<Token> {
+    pos += 2; // Leading '/*'
+    end -= 2; // Trailing '*/'
+
+    if (pos < end && file[pos] === "*") {
+        // Might start with '/**'
+        pos++;
+    }
+
+    // Before skipping whitespace, figure out the comment indent size
+    const [commentHasStars, indent] = discoverIndent(file, pos, end);
+
+    // Skip leading whitespace
+    while (pos < end && /\s/.test(file[pos])) {
+        pos++;
+    }
+
+    // Trailing whitespace
+    while (pos < end && /\s/.test(file[end - 1])) {
+        end--;
+    }
+
+    let lineStart = true;
+
+    for (;;) {
+        if (pos >= end) {
+            return;
+        }
+
+        if (lineStart) {
+            pos = skipIndent(pos);
+            if (commentHasStars && file[pos] === "*") {
+                pos++;
+                if (file[pos] === " ") {
+                    pos++;
+                }
+            }
+            lineStart = false;
+        }
+
+        switch (file[pos]) {
+            case "\n":
+                yield makeToken(TokenSyntaxKind.NewLine, 1);
+                lineStart = true;
+                break;
+
+            case "{":
+                yield makeToken(TokenSyntaxKind.OpenBrace, 1);
+                break;
+
+            case "}":
+                yield makeToken(TokenSyntaxKind.CloseBrace, 1);
+                break;
+
+            case "`": {
+                // Markdown's code rules are a royal pain. This could be one of several things.
+                // 1. Inline code: <1-n ticks><text><same number of ticks>
+                // 2. Code block: <3 ticks><language, no ticks>\n<text>\n<3 ticks>\n
+                // 3. Unmatched tick(s), not code, but part of some text.
+                // We don't quite handle #2 correctly yet. PR welcome!
+                let tickCount = 1;
+                let lookahead = pos;
+
+                while (lookahead + 1 < end && file[lookahead + 1] === "`") {
+                    tickCount++;
+                    lookahead++;
+                }
+                let lookaheadStart = pos;
+                const codeText: string[] = [];
+
+                lookahead++;
+                while (lookahead < end) {
+                    if (lookaheadExactlyNTicks(lookahead, tickCount)) {
+                        lookahead += tickCount;
+                        codeText.push(
+                            file.substring(lookaheadStart, lookahead)
+                        );
+                        yield {
+                            kind: TokenSyntaxKind.Code,
+                            text: codeText.join(""),
+                        };
+                        pos = lookahead;
+                        break;
+                    } else if (file[lookahead] === "`") {
+                        while (lookahead < end && file[lookahead] === "`") {
+                            lookahead++;
+                        }
+                    } else if (
+                        file[lookahead] === "\\" &&
+                        lookahead + 1 < end &&
+                        file[lookahead + 1] !== "\n"
+                    ) {
+                        lookahead += 2;
+                    } else if (file[lookahead] === "\n") {
+                        lookahead++;
+                        codeText.push(
+                            file.substring(lookaheadStart, lookahead)
+                        );
+                        lookahead = skipIndent(lookahead);
+                        if (commentHasStars && file[lookahead] === "*") {
+                            lookahead++;
+                            if (file[lookahead] === " ") {
+                                lookahead++;
+                            }
+                        }
+                        lookaheadStart = lookahead;
+                    } else {
+                        lookahead++;
+                    }
+                }
+
+                if (lookahead >= end && pos !== lookahead) {
+                    if (
+                        tickCount === 3 &&
+                        file.substring(pos, end).includes("\n")
+                    ) {
+                        codeText.push(file.substring(lookaheadStart, end));
+                        yield {
+                            kind: TokenSyntaxKind.Code,
+                            text: codeText.join(""),
+                        };
+                        pos = lookahead;
+                    } else {
+                        yield makeToken(TokenSyntaxKind.Text, tickCount);
+                    }
+                }
+
+                break;
+            }
+
+            case "@": {
+                let lookahead = pos + 1;
+                while (lookahead < end && /[a-z]/i.test(file[lookahead])) {
+                    lookahead++;
+                }
+
+                if (lookahead !== pos + 1) {
+                    while (
+                        lookahead < end &&
+                        /[a-z0-9]/i.test(file[lookahead])
+                    ) {
+                        lookahead++;
+                    }
+                }
+
+                if (lookahead !== pos + 1) {
+                    yield makeToken(TokenSyntaxKind.Tag, lookahead - pos);
+                    break;
+                }
+            }
+            // fall through if we didn't find something that looks like a tag
+
+            default: {
+                const textParts: string[] = [];
+
+                let lookaheadStart = pos;
+                let lookahead = pos;
+                while (lookahead < end) {
+                    if ("{}\n`".includes(file[lookahead])) break;
+
+                    if (
+                        lookahead !== pos &&
+                        file[lookahead] === "@" &&
+                        /\s/.test(file[lookahead - 1])
+                    ) {
+                        // Probably the start of a modifier tag
+                        break;
+                    }
+
+                    if (
+                        file[lookahead] === "\\" &&
+                        lookahead + 1 < end &&
+                        "{}@`".includes(file[lookahead + 1])
+                    ) {
+                        textParts.push(
+                            file.substring(lookaheadStart, lookahead),
+                            file[lookahead + 1]
+                        );
+                        lookahead++;
+                        lookaheadStart = lookahead + 1;
+                    }
+
+                    lookahead++;
+                }
+
+                // If we didn't need to make a copy, don't make one now.
+                if (textParts.length === 0) {
+                    yield makeToken(TokenSyntaxKind.Text, lookahead - pos);
+                } else {
+                    textParts.push(file.substring(lookaheadStart, lookahead));
+                    pos = lookahead;
+                    // This piece of text had line continuations or escaped text, need our own string.
+                    yield {
+                        kind: TokenSyntaxKind.Text,
+                        text: textParts.join(""),
+                    };
+                }
+                break;
+            }
+        }
+    }
+
+    function makeToken(kind: TokenSyntaxKind, size: number): Token {
+        const start = pos;
+        pos += size;
+
+        return {
+            kind,
+            text: file.substr(start, size),
+        };
+    }
+
+    function skipIndent(pos: number) {
+        let taken = indent;
+        let lookahead = pos;
+        while (
+            taken > 0 &&
+            lookahead < end &&
+            file[lookahead] !== "\n" &&
+            /\s/.test(file[lookahead])
+        ) {
+            taken--;
+            lookahead++;
+        }
+        return lookahead;
+    }
+
+    function lookaheadExactlyNTicks(pos: number, n: number) {
+        for (let i = 0; i < n; i++) {
+            if (pos + i === n || file[pos + i] !== "`") {
+                return false;
+            }
+        }
+
+        return pos + n === end || file[pos + n] !== "`";
+    }
+}
+
+function discoverIndent(
+    file: string,
+    pos: number,
+    end: number
+): [boolean, number] {
+    let indent = 0;
+
+    while (pos < end && file[pos] !== "\n") {
+        pos++;
+    }
+
+    outer: while (pos < end) {
+        pos++;
+        const lineStart = pos;
+        while (pos < end && file[pos] !== "\n") {
+            if (/\S/.test(file[pos])) {
+                indent = pos - lineStart;
+                break outer;
+            }
+            pos++;
+        }
+    }
+
+    while (pos < end && /\s/.test(file[pos])) {
+        pos++;
+    }
+
+    const commentHasStars = pos < end && file[pos] === "*";
+
+    return [commentHasStars, indent];
+}
---
+++ src/test/comments.test.ts
@@ -0,0 +1,366 @@
+import { deepStrictEqual as equal } from "assert";
+
+import {
+    lexBlockComment,
+    Token,
+    TokenSyntaxKind,
+} from "../lib/converter/comments";
+
+function dedent(text: string) {
+    const lines = text.split(/\r?\n/);
+    while (lines.length && lines[0].search(/\S/) === -1) {
+        lines.shift();
+    }
+    while (lines.length && lines[lines.length - 1].search(/\S/) === -1) {
+        lines.pop();
+    }
+
+    const minIndent = lines.reduce(
+        (indent, line) => Math.min(indent, line.search(/\S/)),
+        Infinity
+    );
+
+    return lines.map((line) => line.substr(minIndent)).join("\n");
+}
+
+describe("Dedent test helper", () => {
+    it("Works on empty string", () => {
+        equal(dedent(""), "");
+    });
+
+    it("Works with indented text", () => {
+        equal(
+            dedent(`
+            Text here
+        `),
+            "Text here"
+        );
+    });
+
+    it("Works with multiple lines", () => {
+        equal(
+            dedent(`
+            Text here
+                More indented
+        `),
+            "Text here\n    More indented"
+        );
+    });
+});
+
+describe("Block Comment Lexer", () => {
+    function lex(text: string): Token[] {
+        return Array.from(lexBlockComment(text));
+    }
+
+    it("Should handle an empty comment", () => {
+        const tokens = lex("/**/");
+        equal(tokens, []);
+
+        const tokens2 = lex("/***/");
+        equal(tokens2, []);
+
+        const tokens3 = lex("/**  */");
+        equal(tokens3, []);
+    });
+
+    it("Should handle a trivial comment", () => {
+        const tokens = lex("/** Comment */");
+
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "Comment" }]);
+    });
+
+    it("Should handle a multiline comment without stars", () => {
+        const tokens = lex("/* Comment\nNext line */");
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Comment" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "Next line" },
+        ]);
+    });
+
+    it("Should handle a multiline comment with stars", () => {
+        const tokens = lex("/*\n * Comment\n * Next line */");
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Comment" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "Next line" },
+        ]);
+    });
+
+    it("Should handle an indented comment with stars", () => {
+        const tokens = lex(`/**
+            * Text
+            */`);
+
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "Text" }]);
+    });
+
+    it("Should handle an indented comment without stars", () => {
+        const tokens = lex(`/*
+            Text
+            */`);
+
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "Text" }]);
+    });
+
+    it("Should handle a list within a comment without stars", () => {
+        const tokens = lex(
+            dedent(`
+            /*
+             Comment start
+              * This is a list item
+            */
+        `)
+        );
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Comment start" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: " * This is a list item" },
+        ]);
+    });
+
+    it("Should handle higher detected indentation than the rest of the comment", () => {
+        const tokens = lex(
+            dedent(`
+        /*
+             A
+        B
+            */
+        `)
+        );
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "A" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "B" },
+        ]);
+    });
+
+    it("Should handle a comment with stars missing a space", () => {
+        const tokens = lex(
+            dedent(`
+            /*
+             * A
+             *B
+             */
+            `)
+        );
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "A" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "B" },
+        ]);
+    });
+
+    it("Should handle braces", () => {
+        const tokens = lex("/* {} */");
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.OpenBrace, text: "{" },
+            { kind: TokenSyntaxKind.CloseBrace, text: "}" },
+        ]);
+    });
+
+    it("Should handle escaping braces", () => {
+        const tokens = lex("/* \\{\\} */");
+
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "{}" }]);
+    });
+
+    it("Should pass through unknown escapes", () => {
+        const tokens = lex("/* \\\\ \\n */");
+
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "\\\\ \\n" }]);
+    });
+
+    it("Should recognize tags", () => {
+        const tokens = lex("/* @tag @a @abc234 */");
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Tag, text: "@tag" },
+            { kind: TokenSyntaxKind.Text, text: " " },
+            { kind: TokenSyntaxKind.Tag, text: "@a" },
+            { kind: TokenSyntaxKind.Text, text: " " },
+            { kind: TokenSyntaxKind.Tag, text: "@abc234" },
+        ]);
+    });
+
+    it("Should not indiscriminately create tags", () => {
+        const tokens = lex("/* @123 @@ @ */");
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "@123 @@ @" }]);
+    });
+
+    it("Should allow escaping @ to prevent a tag creation", () => {
+        const tokens = lex("/* not a \\@tag */");
+        equal(tokens, [{ kind: TokenSyntaxKind.Text, text: "not a @tag" }]);
+    });
+
+    it("Should not mistake an email for a modifier tag", () => {
+        const tokens = lex("/* test@example.com */");
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "test@example.com" },
+        ]);
+    });
+
+    it("Should allow escaping @ in an email", () => {
+        const tokens = lex("/* test\\@example.com */");
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "test@example.com" },
+        ]);
+    });
+
+    it("Should allow inline code", () => {
+        const tokens = lex("/* test `code` after */");
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "test " },
+            { kind: TokenSyntaxKind.Code, text: "`code`" },
+            { kind: TokenSyntaxKind.Text, text: " after" },
+        ]);
+    });
+
+    it("Should allow inline code with multiple ticks", () => {
+        const tokens = lex("/* test ```not ```` closed``` after */");
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "test " },
+            { kind: TokenSyntaxKind.Code, text: "```not ```` closed```" },
+            { kind: TokenSyntaxKind.Text, text: " after" },
+        ]);
+    });
+
+    it("Should allow escaping ticks", () => {
+        const tokens = lex("/* test `\\`` after */");
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "test " },
+            { kind: TokenSyntaxKind.Code, text: "`\\``" },
+            { kind: TokenSyntaxKind.Text, text: " after" },
+        ]);
+    });
+
+    it("Should handle stars within code", () => {
+        const tokens = lex(
+            dedent(`
+            /**
+             * \`\`\`ts
+             *   test()
+             * \`\`\`
+             */`)
+        );
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Code, text: "```ts\n  test()\n```" },
+        ]);
+    });
+
+    it("Should indent code without stars", () => {
+        const tokens = lex(
+            dedent(`
+            /**
+            \`\`\`ts
+              test()
+            \`\`\`
+            */`)
+        );
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Code, text: "```ts\n  test()\n```" },
+        ]);
+    });
+
+    it("Should treat unclosed inline code as text", () => {
+        const tokens = lex("/* text ` still text */");
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "text ` still text" },
+        ]);
+    });
+
+    it("Should treat unclosed code blocks as code", () => {
+        const tokens = lex(
+            dedent(`
+                /*
+                 * Text
+                 * \`\`\`ts
+                 * foo();
+                 */`)
+        );
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Text" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Code, text: "```ts\nfoo();" },
+        ]);
+    });
+
+    it("Should handle tags after unclosed code", () => {
+        const tokens = lex(
+            dedent(`
+                /*
+                 * Text
+                 * code? \`\` fake
+                 * @blockTag text
+                 */`)
+        );
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Text" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "code? `` fake" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Tag, text: "@blockTag" },
+            { kind: TokenSyntaxKind.Text, text: " text" },
+        ]);
+    });
+
+    it("Should handle text on the first line of a comment", () => {
+        let tokens = lex(
+            dedent(`
+                /* Text
+                 * Text2
+                 */`)
+        );
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Text" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "Text2" },
+        ]);
+
+        tokens = lex(
+            dedent(`
+                /** Text
+                 * Text2
+                 */`)
+        );
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "Text" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "Text2" },
+        ]);
+    });
+
+    it("Should handle a full comment", () => {
+        const tokens = lex(
+            dedent(`
+            /**
+             * This is a summary.
+             *
+             * @remarks
+             * Detailed text here with a {@link Inline | inline link}
+             */`)
+        );
+
+        equal(tokens, [
+            { kind: TokenSyntaxKind.Text, text: "This is a summary." },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Tag, text: "@remarks" },
+            { kind: TokenSyntaxKind.NewLine, text: "\n" },
+            { kind: TokenSyntaxKind.Text, text: "Detailed text here with a " },
+            { kind: TokenSyntaxKind.OpenBrace, text: "{" },
+            { kind: TokenSyntaxKind.Tag, text: "@link" },
+            { kind: TokenSyntaxKind.Text, text: " Inline | inline link" },
+            { kind: TokenSyntaxKind.CloseBrace, text: "}" },
+        ]);
+    });
+});
