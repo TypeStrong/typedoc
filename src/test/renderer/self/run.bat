0dd2688954ebec5517c552f38c25dd851fa3e38b
Sam Magura <srmagura@gmail.com>
2021-09-29 09:45:38 -0400

    1556: remove src/test/renderer/self per @Gerrit0

--- src/test/renderer/self/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set curr_dir=%cd%
-chdir /D "%~dp0"
-
-node ..\..\..\..\bin\typedoc --name "TypeDoc Documentation" --tsconfig ../../../../tsconfig.json --readme ../../../../README.md --out doc\ --entryPointStrategy expand ..\..\..\..\src\lib\
-
-chdir /D "%curr_dir%"
