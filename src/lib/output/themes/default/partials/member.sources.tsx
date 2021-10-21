445129f7d1ee4be888f7174695739abd1cbf4444
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-10-21 13:48:56 +0200

    Removed empty source element

--- src/lib/output/themes/default/partials/member.sources.tsx
+++ src/lib/output/themes/default/partials/member.sources.tsx
@@ -5,43 +5,47 @@ import type { DeclarationReflection, SignatureReflection } from "../../../../mod
 export const memberSources = (
     context: DefaultThemeRenderContext,
     props: SignatureReflection | DeclarationReflection
-) => (
-    <aside class="tsd-sources">
-        {!!props.implementationOf && (
-            <p>
-                {"Implementation of "}
-                {context.typeAndParent(props.implementationOf)}
-            </p>
-        )}
-        {!!props.inheritedFrom && (
-            <p>
-                {"Inherited from "}
-                {context.typeAndParent(props.inheritedFrom)}
-            </p>
-        )}
-        {!!props.overwrites && (
-            <p>
-                {"Overrides "}
-                {context.typeAndParent(props.overwrites)}
-            </p>
-        )}
-        {!!props.sources && (
-            <ul>
-                {props.sources.map((item) =>
-                    item.url ? (
-                        <li>
-                            {"Defined in "}
-                            <a href={item.url}>
-                                {item.fileName}:{item.line}
-                            </a>
-                        </li>
-                    ) : (
-                        <li>
-                            Defined in {item.fileName}:{item.line}
-                        </li>
-                    )
-                )}
-            </ul>
-        )}
-    </aside>
-);
+) => {
+    const element = (
+        <aside class="tsd-sources">
+            {!!props.implementationOf && (
+                <p>
+                    {"Implementation of "}
+                    {context.typeAndParent(props.implementationOf)}
+                </p>
+            )}
+            {!!props.inheritedFrom && (
+                <p>
+                    {"Inherited from "}
+                    {context.typeAndParent(props.inheritedFrom)}
+                </p>
+            )}
+            {!!props.overwrites && (
+                <p>
+                    {"Overrides "}
+                    {context.typeAndParent(props.overwrites)}
+                </p>
+            )}
+            {!!props.sources && (
+                <ul>
+                    {props.sources.map((item) =>
+                        item.url ? (
+                            <li>
+                                {"Defined in "}
+                                <a href={item.url}>
+                                    {item.fileName}:{item.line}
+                                </a>
+                            </li>
+                        ) : (
+                            <li>
+                                Defined in {item.fileName}:{item.line}
+                            </li>
+                        )
+                    )}
+                </ul>
+            )}
+        </aside>
+    );
+
+    return <>{element.children.some((c) => !!c) && element}</>;
+};
