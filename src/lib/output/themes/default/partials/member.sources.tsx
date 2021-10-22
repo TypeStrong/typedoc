8e9334d0c459ad3da268b8a84162022a544d5680
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-10-22 13:38:41 +0200

    Use array

--- src/lib/output/themes/default/partials/member.sources.tsx
+++ src/lib/output/themes/default/partials/member.sources.tsx
@@ -6,46 +6,56 @@ export const memberSources = (
     context: DefaultThemeRenderContext,
     props: SignatureReflection | DeclarationReflection
 ) => {
-    const element = (
-        <aside class="tsd-sources">
-            {!!props.implementationOf && (
-                <p>
-                    {"Implementation of "}
-                    {context.typeAndParent(props.implementationOf)}
-                </p>
-            )}
-            {!!props.inheritedFrom && (
-                <p>
-                    {"Inherited from "}
-                    {context.typeAndParent(props.inheritedFrom)}
-                </p>
-            )}
-            {!!props.overwrites && (
-                <p>
-                    {"Overrides "}
-                    {context.typeAndParent(props.overwrites)}
-                </p>
-            )}
-            {!!props.sources && (
-                <ul>
-                    {props.sources.map((item) =>
-                        item.url ? (
-                            <li>
-                                {"Defined in "}
-                                <a href={item.url}>
-                                    {item.fileName}:{item.line}
-                                </a>
-                            </li>
-                        ) : (
-                            <li>
-                                Defined in {item.fileName}:{item.line}
-                            </li>
-                        )
-                    )}
-                </ul>
-            )}
-        </aside>
-    );
+    const sources: JSX.Element[] = [];
 
-    return <>{element.children.some((c) => !!c) && element}</>;
+    if (props.implementationOf) {
+        sources.push(
+            <p>
+                {"Implementation of "}
+                {context.typeAndParent(props.implementationOf)}
+            </p>
+        );
+    }
+    if (props.inheritedFrom) {
+        sources.push(
+            <p>
+                {"Inherited from "}
+                {context.typeAndParent(props.inheritedFrom)}
+            </p>
+        );
+    }
+    if (props.overwrites) {
+        sources.push(
+            <p>
+                {"Overrides "}
+                {context.typeAndParent(props.overwrites)}
+            </p>
+        );
+    }
+    if (props.sources) {
+        sources.push(
+            <ul>
+                {props.sources.map((item) =>
+                    item.url ? (
+                        <li>
+                            {"Defined in "}
+                            <a href={item.url}>
+                                {item.fileName}:{item.line}
+                            </a>
+                        </li>
+                    ) : (
+                        <li>
+                            Defined in {item.fileName}:{item.line}
+                        </li>
+                    )
+                )}
+            </ul>
+        );
+    }
+
+    if (sources.length === 0) {
+        return <></>;
+    }
+
+    return <aside class="tsd-sources">{sources}</aside>;
 };
