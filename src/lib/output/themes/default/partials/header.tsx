ca63d69aafcfd384e94dec5931fe45b82cfd1254
Futuristick <me@futuristick.ca>
2021-11-14 13:19:21 -0500

    Clean up toolbar widgets

--- src/lib/output/themes/default/partials/header.tsx
+++ src/lib/output/themes/default/partials/header.tsx
@@ -3,18 +3,18 @@ import { JSX } from "../../../../utils";
 import type { PageEvent } from "../../../events";
 import { hasTypeParameters, join } from "../../lib";
 import type { DefaultThemeRenderContext } from "../DefaultThemeRenderContext";
+import { icons } from "./icon";
 
 export const header = (context: DefaultThemeRenderContext, props: PageEvent<Reflection>) => (
     <header>
         <div class="tsd-page-toolbar">
-            <div class="container">
-                <div class="table-wrap">
+                <div class="tsd-toolbar-contents container">
                     <div class="table-cell" id="tsd-search" data-base={context.relativeURL("./")}>
                         <div class="field">
                             <label for="tsd-search-field" class="tsd-widget search no-caption">
-                                Search
+                                {icons.search()}
                             </label>
-                            <input type="text" id="tsd-search-field" />
+                            <input type="text" id="tsd-search-field" aria-label="Search" />
                         </div>
 
                         <ul class="results">
@@ -28,12 +28,11 @@ export const header = (context: DefaultThemeRenderContext, props: PageEvent<Refl
                     </div>
 
                     <div class="table-cell" id="tsd-widgets">
-                        <a href="#" class="tsd-widget menu no-caption" data-toggle="menu">
-                            Menu
+                        <a href="#" class="tsd-widget menu no-caption" data-toggle="menu" aria-label="Menu">
+                            {icons.menu()}
                         </a>
                     </div>
                 </div>
-            </div>
         </div>
         <div class="tsd-page-title">
             <div class="container">
--- src/lib/output/themes/default/partials/icon.tsx
+++ src/lib/output/themes/default/partials/icon.tsx
@@ -1,10 +1,14 @@
 import { ReflectionKind } from "../../../../models";
 import { JSX } from "../../../../utils";
 
-interface UtilityIcons {
-    chevronDown: () => JSX.Element;
-    checkbox: () => JSX.Element;
-}
+type UtilityIcons = Record<
+    | 'chevronDown'
+    | 'checkbox'
+    | 'menu'
+    | 'search',
+    () => JSX.Element
+>;
+
 export const icons: Record<ReflectionKind, () => JSX.Element | null> & UtilityIcons = {
     [ReflectionKind.All]: () => null,
     [ReflectionKind.Accessor]: () => (
@@ -211,7 +215,7 @@ export const icons: Record<ReflectionKind, () => JSX.Element | null> & UtilityIc
         return this[ReflectionKind.Variable]()
     },
     chevronDown: () => (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
             <path d="M5.46899 9.53118L12.53 16.5912L19.591 9.53118L17.469 7.40918L12.53 12.3492L7.591 7.40918L5.46899 9.53118Z" fill="var(--color-text)"/>
         </svg>
     ),
@@ -220,6 +224,18 @@ export const icons: Record<ReflectionKind, () => JSX.Element | null> & UtilityIc
             <rect class="tsd-checkbox-background" width="32" height="32" fill="none"/>
             <path class="tsd-checkbox-checkmark" d="M8.35422 16.8214L13.2143 21.75L24.6458 10.25" stroke="none"
                   stroke-width="3.5" stroke-linejoin="round" fill="none"/>
+        </svg>
+    ),
+    menu: () => (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+            {["3", "7", "11"].map(
+                y => <rect x="1" y={y} width="14" height="2" fill="var(--color-text)"/>
+            )}
+        </svg>
+    ),
+    search: () => (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M15.7824 13.833L12.6666 10.7177C12.5259 10.5771 12.3353 10.499 12.1353 10.499H11.6259C12.4884 9.39596 13.001 8.00859 13.001 6.49937C13.001 2.90909 10.0914 0 6.50048 0C2.90959 0 0 2.90909 0 6.49937C0 10.0896 2.90959 12.9987 6.50048 12.9987C8.00996 12.9987 9.39756 12.4863 10.5008 11.6239V12.1332C10.5008 12.3332 10.5789 12.5238 10.7195 12.6644L13.8354 15.7797C14.1292 16.0734 14.6042 16.0734 14.8948 15.7797L15.7793 14.8954C16.0731 14.6017 16.0731 14.1267 15.7824 13.833ZM6.50048 10.499C4.29094 10.499 2.50018 8.71165 2.50018 6.49937C2.50018 4.29021 4.28781 2.49976 6.50048 2.49976C8.71001 2.49976 10.5008 4.28708 10.5008 6.49937C10.5008 8.70852 8.71314 10.499 6.50048 10.499Z" fill="var(--color-text)"/>
         </svg>
     )
 }
