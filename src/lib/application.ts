7072f737188f6c053a16a6d4e25105190b5f40d3
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2022-10-23 11:21:59 -0600

    Adjust unsupported warning

--- src/lib/application.ts
+++ src/lib/application.ts
@@ -211,7 +211,7 @@ export class Application extends ChildableComponent<
             )
         ) {
             this.logger.warn(
-                `You are running with an unsupported TypeScript version! This may work, or it might break. TypeDoc supports ${supportedVersionMajorMinor.join(
+                `You are running with an unsupported TypeScript version! If TypeDoc crashes, this is why. TypeDoc supports ${supportedVersionMajorMinor.join(
                     ", "
                 )}`
             );
