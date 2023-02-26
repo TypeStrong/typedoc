6ee136acbf5e98f93652a13a2d4206685321f82c
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-02-27 04:15:25 +1300

    feat: add validator run event to allow plugins to perform custom validation (#2184)
    * feat: add validator run event to allow plugins to perform custom validation

--- src/lib/application.ts
+++ src/lib/application.ts
@@ -108,6 +108,12 @@ export class Application extends ChildableComponent<
     static readonly EVENT_BOOTSTRAP_END = ApplicationEvents.BOOTSTRAP_END;
 
     /**
+     * Emitted when validation is being run.
+     * The listener will be given an instance of {@link ProjectReflection}.
+     */
+    static readonly EVENT_VALIDATE_PROJECT = ApplicationEvents.VALIDATE_PROJECT;
+
+    /**
      * Create a new TypeDoc application instance.
      */
     constructor() {
@@ -430,6 +436,8 @@ export class Application extends ChildableComponent<
         if (checks.invalidLink) {
             validateLinks(project, this.logger);
         }
+
+        this.trigger(Application.EVENT_VALIDATE_PROJECT, project);
 
         this.logger.verbose(`Validation took ${Date.now() - start}ms`);
     }
