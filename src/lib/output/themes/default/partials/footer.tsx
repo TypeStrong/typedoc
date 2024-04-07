b53300433f288c8adbb87f21480f56fcc7367f56
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2024-04-07 15:47:56 -0600

    Move generator into `<footer>`
    Resolves #2532

--- src/lib/output/themes/default/partials/footer.tsx
+++ src/lib/output/themes/default/partials/footer.tsx
@@ -3,15 +3,18 @@ import type { DefaultThemeRenderContext } from "../DefaultThemeRenderContext";
 
 export function footer(context: DefaultThemeRenderContext) {
     const hideGenerator = context.options.getValue("hideGenerator");
-    if (!hideGenerator)
-        return (
-            <div class="tsd-generator">
-                <p>
+    return (
+        <footer>
+            {context.hook("footer.begin")}
+            {hideGenerator || (
+                <p class="tsd-generator">
                     {"Generated using "}
                     <a href="https://typedoc.org/" target="_blank">
                         TypeDoc
                     </a>
                 </p>
-            </div>
-        );
+            )}
+            {context.hook("footer.end")}
+        </footer>
+    );
 }
