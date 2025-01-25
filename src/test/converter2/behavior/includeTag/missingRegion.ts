156338238e97486eb03574b098d33caaf19bc614
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2025-01-24 20:09:32 -0700

    Add unit tests for include/includeCode with regions

---
+++ src/test/converter2/behavior/includeTag/missingRegion.ts
@@ -0,0 +1,9 @@
+// #region missingEnd
+// #endregion missingStart
+
+/**
+ * {@includeCode missingRegion.ts#noRegion}
+ * {@includeCode missingRegion.ts#missingStart}
+ * {@includeCode missingRegion.ts#missingEnd}
+ */
+export const includeWarnings = 456;
