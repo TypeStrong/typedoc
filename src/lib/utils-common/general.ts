8ff7d27bb43adb4c9bf42485edbdd59f504428ba
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2025-09-27 22:50:07 -0600

    Fix lint

--- src/lib/utils-common/general.ts
+++ src/lib/utils-common/general.ts
@@ -45,6 +45,7 @@ export function assertNever(x: never): never {
 
 export function assert(x: unknown, message = "Assertion failed"): asserts x {
     if (!x) {
+        // eslint-disable-next-line no-debugger
         debugger;
         throw new Error(message);
     }
